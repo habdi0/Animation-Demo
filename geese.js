f941c39ea051bc7d7eb189fe83d8a7ffb8179e57
habdi0 <habdinur@uw.edu>
2023-01-22 23:20:03 -0800

    Now goose walks across screen in a loop.

--- geese.js
+++ geese.js
@@ -3,13 +3,14 @@ class Geese {
         this.game = game;
         this.animator = new Animator(ASSET_MANAGER.getAsset("./Geese.png"), 0, 100, 48, 48, 4, 0.5);
 
-        this.x = 0;
-        this.y = 0;
-        this.speed = 25;
+        this.x = -250;
+        this.y = 200;
+        this.speed = 200;
     };
 
     update() {
         this.x += this.speed * this.game.clockTick;
+        if (this.x > 1024) this.x = -250;
     };
 
     draw(ctx) { // ctx - the context
