f5e43baf23884079b422f23559270c5efad0b5dc
habdi0 <habdinur@uw.edu>
2023-01-22 23:10:25 -0800

    Now goose walks to the right instead of forward

--- geese.js
+++ geese.js
@@ -1,14 +1,19 @@
 class Geese {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./Geese.png"), 0, 0, 48, 48, 3, 0.5);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./Geese.png"), 0, 100, 48, 48, 4, 0.5);
+
+        this.x = 0;
+        this.y = 0;
+        this.speed = 25;
     };
 
     update() {
-
+        this.x += this.speed * this.game.clockTick;
     };
 
     draw(ctx) { // ctx - the context
-        this.animator.drawFrame(this.game.clockTick, ctx, 300, 200);
+        // this.animator.drawFrame(this.game.clockTick, ctx, 0, 0);
+        this.animator.drawFrame(this.game.clockTick, ctx, this.x, this.y);
     };
 }
